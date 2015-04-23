1e783ec3acc78d1526c9cfa861895000af015225
Lee Pollard <tjdet11@gmail.com>
2015-04-23 00:32:03 -0400

    -Added initial app files

---
+++ Tendigi_Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tendigi_Twitter
+//
+//  Created by Lee Pollard on 4/23/15.
+//  Copyright (c) 2015 Tendigi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tendigi_Twitter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tendigi_Twitter
+//
+//  Created by Lee Pollard on 4/23/15.
+//  Copyright (c) 2015 Tendigi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
