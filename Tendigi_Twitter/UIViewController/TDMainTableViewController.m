e1456fd49448207f3ba695b8ecbd07e5d911e06b
Lee Pollard <tjdet11@gmail.com>
2015-04-23 00:43:33 -0400

    -Added fetch JSON Twitter data -Use fetched Twitter data in UI

--- Tendigi_Twitter/UIViewController/TDMainTableViewController.m
+++ Tendigi_Twitter/UIViewController/TDMainTableViewController.m
@@ -7,57 +7,172 @@
 //
 
 #import "TDMainTableViewController.h"
+#import <TwitterKit/TwitterKit.h>
 
 
 
 
 
-@interface TDMainTableViewController ()
+static NSString * const TweetTableReuseIdentifier = @"TweetCell";
+
+
+
+
+
+@interface TDMainTableViewController () <TWTRTweetViewDelegate>
+
+@property (nonatomic, strong) NSArray *tweets;
+@property (nonatomic, strong) NSMutableArray *loadedTweetIDs;
+-(void)loadTweets;
+-(void)guestLogin;
+-(void)loadWithTweetIDs;
 
 @end
 
+
+
+
+
 @implementation TDMainTableViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
 	
-	[self.view setBackgroundColor:[UIColor whiteColor]];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+	//	NSArray *tweetIDs = @[@"20", // @jack's first Tweet
+	//						  @"510908133917487104" // our favorite Bike tweet
+	//						  ];
+	
+	self.tableView.estimatedRowHeight = 150;
+	self.tableView.rowHeight = UITableViewAutomaticDimension;
+	
+	self.tableView.allowsSelection = NO;
+	[self.tableView registerClass:[TWTRTweetTableViewCell class] forCellReuseIdentifier:TweetTableReuseIdentifier];
+	
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)viewWillAppear:(BOOL)animated
+{
+	[super viewWillAppear:YES];
+	
+	_loadedTweetIDs = [NSMutableArray new];
+	
+	[self guestLogin];
+}
+
+#pragma mark - Load Tweets
+-(void)guestLogin
+{
+	[[Twitter sharedInstance] logInGuestWithCompletion:^(TWTRGuestSession *guestSession, NSError *error)
+	 {
+		 if (guestSession)
+		 {
+			 [self loadTweets];
+		 }
+		 else
+		 {
+			 NSLog(@"error: %@", [error localizedDescription]);
+		 }
+	 }];
+}
+
+-(void)loadTweets
+{
+	NSString *statusesShowEndpoint = @"https://api.twitter.com/1.1/statuses/lookup.json";
+	NSDictionary *params = @{@"id" : @"510908133917487104"};
+	NSError *clientError;
+	NSURLRequest *request = [[[Twitter sharedInstance] APIClient]
+							 URLRequestWithMethod:@"GET"
+							 URL:statusesShowEndpoint
+							 parameters:params
+							 error:&clientError];
+ 
+	if (request) {
+		[[[Twitter sharedInstance] APIClient]
+		 sendTwitterRequest:request
+		 completion:^(NSURLResponse *response,
+					  NSData *data,
+					  NSError *connectionError) {
+			 
+			 
+			 if (data) {
+				 // handle the response data e.g.
+				 NSError *jsonError;
+				 NSArray *json = [NSJSONSerialization
+								  JSONObjectWithData:data
+								  options:0
+								  error:&jsonError];
+				 
+				 NSDictionary *json1 = [json objectAtIndex:0];
+				 if ([json1 isKindOfClass:[NSDictionary class]])
+				 {
+					 TWTRTweet *tweet = [[TWTRTweet alloc] initWithJSONDictionary:json1];
+					 if (tweet)
+					 {
+						 NSString *tweetID = tweet.tweetID;
+						 [self.loadedTweetIDs addObject:tweetID];
+						 [self loadWithTweetIDs];
+					 }
+				 }
+			 }
+			 else {
+				 NSLog(@"Error: %@", connectionError);
+			 }
+		 }];
+	}
+	else {
+		NSLog(@"Error: %@", clientError);
+	}
+}
+
+-(void)loadWithTweetIDs
+{
+	__weak typeof(self) weakSelf = self;
+	[[[Twitter sharedInstance] APIClient] loadTweetsWithIDs:self.loadedTweetIDs completion:^(NSArray *tweets, NSError *error) {
+		if (tweets) {
+			typeof(self) strongSelf = weakSelf;
+			strongSelf.tweets = tweets;
+			[strongSelf.tableView reloadData];
+		} else {
+			NSLog(@"Failed to load tweet: %@", [error localizedDescription]);
+		}
+	}];
 }
 
 #pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+	// Return the number of sections.
+	return 1;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+	// Return the number of rows in the section.
+	return self.tweets.count;
 }
 
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    return cell;
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	TWTRTweet *tweet = self.tweets[indexPath.row];
+ 
+	TWTRTweetTableViewCell *cell = (TWTRTweetTableViewCell *)
+	
+	[self.tableView dequeueReusableCellWithIdentifier:TweetTableReuseIdentifier forIndexPath:indexPath];
+	
+	[cell configureWithTweet:tweet];
+	cell.tweetView.delegate = self;
+ 
+	return cell;
 }
-*/
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+	TWTRTweet *tweet = self.tweets[indexPath.row];
+ 
+	return [TWTRTweetTableViewCell heightForTweet:tweet width:CGRectGetWidth(self.view.bounds)];
+}
+
 
 /*
 // Override to support conditional editing of the table view.
